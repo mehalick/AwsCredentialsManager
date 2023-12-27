91448e20689aaa98e34b068550c5276a2d560c64
Andy Mehalick <andy.mehalick@outlook.com>
2023-12-27 07:55:41 -0500

    initial commit

---
+++ AwsCredentialsManager/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace AwsCredentialsManager;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AwsCredentialsManager/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AwsCredentialsManager;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
